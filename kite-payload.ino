8ba35069a638470f33d2899bd2dce91737a28c27
Wesley Ellis <tahnok@gmail.com>
2019-05-16 22:18:40 -0400

    add auto incrementing datafile support

--- kite-payload.ino
+++ kite-payload.ino
@@ -8,6 +8,8 @@ const int sdChipSelect = 10;
 const char dataFile[] = "log.csv";
 
 Adafruit_BMP280 altimeter; // I2C
+char filename[13];
+uint32_t time = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -23,9 +25,11 @@ void setup() {
   }
   Serial.println("altimeter started");
 
-}
+  findFilename(filename);
+  Serial.print("writing data to ");
+  Serial.println(filename);
 
-uint32_t time = 0;
+}
 
 void loop() {
   float pressure = altimeter.readPressure();
@@ -36,21 +40,20 @@ void loop() {
 }
 
 void logData(uint32_t time, float pressure, float temperature) {
-  File dataFile = SD.open("DATA2.CSV", FILE_WRITE);
-  if (dataFile) {
-    dataFile.print(time);
-    dataFile.print(",");
-    dataFile.print(pressure);
-    dataFile.print(",");
-    dataFile.print(temperature);
-    dataFile.println();
-    dataFile.close();
-    Serial.println("done");
-  }
-  // if the file isn't open, pop up an error:
-  else {
+  File dataFile = SD.open(filename, FILE_WRITE);
+  if (!dataFile) {
     error("error opening datalog.txt");
   }
+
+  dataFile.print(time);
+  dataFile.print(",");
+  dataFile.print(pressure);
+  dataFile.print(",");
+  dataFile.print(temperature);
+  dataFile.println();
+  dataFile.close();
+  Serial.println("done");
+
 }
 
 void error(char *errorMessage) {
@@ -60,3 +63,28 @@ void error(char *errorMessage) {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   while (1) {}
 }
+
+/*
+   Populates buffer with filename
+   in the format FLIGHTXX.CSV
+   where XX is a number between 00 and 99
+
+   this method looks through the root dir of the SD card for
+   any existing files in that format and returns XX + 1
+*/
+void findFilename(char* filename) {
+  strcpy(filename, "FLIGHT00.csv");
+  for (uint8_t i = 0; i < 100; i++) {
+    uint8_t offset = 6 ;
+    if (i < 10) {
+      offset = 7;
+    }
+    itoa(i, filename + offset * sizeof(char), 10);
+
+    filename[8] = '.'; //clear null termination from itoa
+    if (!(SD.exists(filename))) {
+      break;
+    }
+  }
+}
+
